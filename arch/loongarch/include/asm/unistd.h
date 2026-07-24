034c404696d90902dc4affd96b1519b471b36135
eWloYW8 <3171132517@qq.com>
2026-07-24 02:05:41 +0000

    fix(loongarch): restore legacy LA32R syscall ABIs

--- arch/loongarch/include/asm/unistd.h
+++ arch/loongarch/include/asm/unistd.h
@@ -6,6 +6,12 @@
  * Copyright (C) 2020-2022 Loongson Technology Corporation Limited
  */
 
+#ifdef CONFIG_32BIT
+#define __ARCH_WANT_STAT64
+#define __ARCH_WANT_TIME32_SYSCALLS
+#define __ARCH_WANT_SET_GET_RLIMIT
+#endif
+
 #include <uapi/asm/unistd.h>
 
 #define __ARCH_WANT_NEW_STAT
